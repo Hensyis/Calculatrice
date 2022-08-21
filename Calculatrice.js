e301eb36616ddeba1d081840af3e8c7a9badfce1
Jacques <zhou.jacques27@gmail.com>
2022-08-21 18:21:52 +0200

    Ajouter des fonctions
    Fonction qui récupère la première opérande, deuxième opérande, opérateur
    Fonction qui permet de réinitialiser

--- Calculatrice.js
+++ Calculatrice.js
@@ -1,6 +1,6 @@
-const add = (a,b) => a + b;
+const add = (a,b) =>  a + b;
 
-const soustraction = (a,b) => a + b;
+const soustraction = (a,b) => a - b;
 
 const pourcentage = a => a/100;
 
@@ -36,15 +36,57 @@ const container = document.querySelector('#container');
 const touchesNombre = container.querySelectorAll('.toucheNombre');
 const p = container.querySelector('#ecran p');
 const afficherNombre1 = touchesNombre.forEach(toucheNombre => toucheNombre.addEventListener('click', function(e){
-    nombre1 = nombre1 + e.target.lastChild.textContent;
-    console.log(nombre1);
-    p.textContent = nombre1;
-}));
+    if(operateur === ""){nombre1 = nombre1 + e.target.lastChild.textContent;
+        console.log(nombre1);
+        p.textContent = nombre1;
+}}));
 //fin --------------------------------------------------
 
 //début cliquer sur un opérateur
-const 
+const touchesOperateur = container.querySelectorAll('.toucheOperateur');
+const sauvegarderOperateur = touchesOperateur.forEach(toucheOperateur => toucheOperateur.addEventListener('click',
+(e) => {
+    if(operateur === ""){
+        operateur = e.target.innerText;
+        p.textContent = "";
+    }
+    else if(operateur !== ""){
+        operateur = "";
+        operateur = e.target.innerText;
+    }
+}));
+//fin ----------------------------------------------------
 
+//début afficher nombre2 lors de clique sur une touche nombre
+const afficherNombre2 = touchesNombre.forEach(toucheNombre => toucheNombre.addEventListener('click', function(e){
+    if(operateur !== ""){
+        nombre2 = nombre2 + e.target.lastChild.textContent;
+    console.log(nombre2);
+    p.textContent = nombre2;
+}}));
+//fin -----------------------------------------------------
 
+//début calculer l'operation entre les deux nombres
+const toucheEgalite = container.querySelector('.afficher');
+const afficherResultat = toucheEgalite.addEventListener('click',()=>{
+    if(nombre3 === ""){
+        let nb1 = parseInt(nombre1);
+        let nb2 = parseInt(nombre2);
+        nombre3 = `${operation(operateur, nb1, nb2)}`;
+        console.log(nombre3);
+        p.textContent = nombre3;
+    }
+});
 
-const afficherResultat = document.querySelector('.afficher');
+//fin ------------------------------------------------------
+
+//début bouton clear
+const toucheClear = container.querySelector('#toucheClear');
+const effacerTout = toucheClear.addEventListener('click', ()=>{
+    nombre1 = "";
+    nombre2 = "";
+    nombre3 = "";
+    operateur = "";
+    p.textContent = "";
+});
+//fin -------------------------------------------------------
