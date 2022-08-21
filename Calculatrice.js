731b43fbae4d35b57b2291c812a3d110a5391b44
Jacques <zhou.jacques27@gmail.com>
2022-08-21 20:10:17 +0200

    caract: ajouter enchaînement d'operation
    Jusqu'à présent nous avons pu que faire des opérations du style
    2+2=4 or maintenant nous pouvons faire par exemple un enchaînement
    de 12+7-5*3=42

--- Calculatrice.js
+++ Calculatrice.js
@@ -36,10 +36,17 @@ const container = document.querySelector('#container');
 const touchesNombre = container.querySelectorAll('.toucheNombre');
 const p = container.querySelector('#ecran p');
 const afficherNombre1 = touchesNombre.forEach(toucheNombre => toucheNombre.addEventListener('click', function(e){
-    if(operateur === ""){nombre1 = nombre1 + e.target.lastChild.textContent;
+    if(operateur === ""){
+        nombre1 = nombre1 + e.target.lastChild.textContent;
         console.log(nombre1);
         p.textContent = nombre1;
-}}));
+    }
+    else if(nombre3 !== ""){
+        nombre1 = nombre3;
+    }
+
+
+}));
 //fin --------------------------------------------------
 
 //début cliquer sur un opérateur
@@ -50,9 +57,21 @@ const sauvegarderOperateur = touchesOperateur.forEach(toucheOperateur => toucheO
         operateur = e.target.innerText;
         p.textContent = "";
     }
-    else if(operateur !== ""){
+    else if(operateur !== "" && nombre2 === ""){
         operateur = "";
+        p.textContent = "";
         operateur = e.target.innerText;
+    }
+    else if(operateur !== "" && nombre1 !== "" && nombre2 !== ""){
+        nombre3 = `${operation(operateur, parseInt(nombre1), parseInt(nombre2))}`;
+        console.log(nombre3);
+        operateur = "";
+        p.textContent = "";
+        p.textContent = nombre3;
+        operateur = e.target.innerText;
+        nombre1 = "";
+        nombre2 = "";
+
     }
 }));
 //fin ----------------------------------------------------
@@ -61,21 +80,26 @@ const sauvegarderOperateur = touchesOperateur.forEach(toucheOperateur => toucheO
 const afficherNombre2 = touchesNombre.forEach(toucheNombre => toucheNombre.addEventListener('click', function(e){
     if(operateur !== ""){
         nombre2 = nombre2 + e.target.lastChild.textContent;
-    console.log(nombre2);
-    p.textContent = nombre2;
-}}));
+        console.log(nombre2);
+        p.textContent = nombre2;
+    }
+    else if(nombre3 !== ""){
+        nombre3 = "";
+        nombre2 = nombre2 + e.target.lastChild.textContent;
+        console.log(nombre2);
+        p.textContent = nombre2;
+    }
+
+}));
 //fin -----------------------------------------------------
 
 //début calculer l'operation entre les deux nombres
 const toucheEgalite = container.querySelector('.afficher');
 const afficherResultat = toucheEgalite.addEventListener('click',()=>{
-    if(nombre3 === ""){
-        let nb1 = parseInt(nombre1);
-        let nb2 = parseInt(nombre2);
-        nombre3 = `${operation(operateur, nb1, nb2)}`;
-        console.log(nombre3);
-        p.textContent = nombre3;
-    }
+    
+    nombre3 = `${operation(operateur, parseInt(nombre1), parseInt(nombre2))}`;;
+    console.log(nombre3);
+    p.textContent = nombre3;
 });
 
 //fin ------------------------------------------------------
